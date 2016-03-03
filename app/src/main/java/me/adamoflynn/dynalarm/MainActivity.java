96ae3c421cf9ad1f1af7bc2f6084702f6c1694a2
Adam O'Flynn <adam.oflynn7@mail.dcu.ie>
2016-03-03 15:32:12 +0000

    Accelerometer now reads motion of device. Flat on a table - reads no motion. If I hit table, motion detected. Will need a lot of user testing, but acceptable for now.

--- app/src/main/java/me/adamoflynn/dynalarm/MainActivity.java
+++ app/src/main/java/me/adamoflynn/dynalarm/MainActivity.java
@@ -6,19 +6,22 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.DropBoxManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.FloatMath;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Map;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -33,29 +36,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 	private Sensor mAccelerometer;
 	private final String FILE_DIR = "data/data/me.adamoflynn.dynalarm/files/";
 	private final String FILE_NAME = "sleep_file";
-	private int id = 0;
+	private int i = 0;
 	private long lastUpdate = 0;
-	private TextView acclX, acclY, acclZ;
+	private float[] mGravity;
+	private float accel, accelCurrent, accelLast;
+	private TextView acclX, acclY, acclZ, motion;
+	private LineChart chart;
+	private ArrayList<Entry> entries = new ArrayList<>();
+	private ArrayList<String> labels = new ArrayList<>();
+	private float x, y, z, lastX, lastY, lastZ;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		RealmConfiguration config = new RealmConfiguration.Builder(this)
-				.name("default")
-				.schemaVersion(1)
-				.deleteRealmIfMigrationNeeded()
-				.build();
-
-		db = Realm.getInstance(config);
-
-
+		dbSetup();
 		initializeViews();
-
+		//initializeChart();
 
 		mSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
-		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
 	}
 
 
@@ -65,17 +67,37 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 	public void onStopClick(View view) {
 		mSensorManager.unregisterListener(this);
-		RealmResults<AccelerometerData> results = db.where(AccelerometerData.class).findAll();
-		for (AccelerometerData a:results ) {
-			Log.d("Data: ", a.toString());
-		}
-
+		initializeChart();
+		Log.d("Size of data:", Integer.toString(entries.size()));
 	}
 
 	public void initializeViews(){
 		acclX = (TextView) findViewById(R.id.acclX);
-		acclY = (TextView) findViewById(R.id.acclY);
-		acclZ = (TextView) findViewById(R.id.acclZ);
+		//acclY = (TextView) findViewById(R.id.acclY);
+		//acclZ = (TextView) findViewById(R.id.acclZ);
+		motion = (TextView) findViewById(R.id.motion);
+	}
+
+	public void initializeChart(){
+		LineChart chart = (LineChart)findViewById(R.id.chart);
+		LineDataSet dataSet = new LineDataSet(entries, "movement/time");
+		dataSet.setDrawCubic(true);
+		dataSet.setDrawFilled(true);
+		dataSet.setFillColor(1);
+
+		LineData data = new LineData(labels, dataSet);
+		chart.setData(data);
+		chart.invalidate();
+	}
+
+	protected void dbSetup(){
+		RealmConfiguration config = new RealmConfiguration.Builder(this)
+				.name("default")
+				.schemaVersion(1)
+				.deleteRealmIfMigrationNeeded()
+				.build();
+
+		db = Realm.getInstance(config);
 	}
 
 	@Override
@@ -105,25 +127,37 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-		float x = event.values[0];
-		float y = event.values[1];
-		float z = event.values[2];
-
+		x = event.values[0];
+		y = event.values[1];
+		z = event.values[2];
 		long curTime = System.currentTimeMillis();
 
-		if ((curTime - lastUpdate) > 5000) {
-			Log.d(" Data", x + "," + y + "," + z + "\n" );
+		if ((curTime - lastUpdate) > 100) {
 			lastUpdate = curTime;
-			acclX.setText(Float.toString(x));
-			acclY.setText(Float.toString(y));
-			acclZ.setText(Float.toString(z));
+			long curTimeSec = curTime/1000;
+			mGravity = event.values.clone();
+			accelLast = accelCurrent;
+			accelCurrent = (float)Math.sqrt(x*x + y*y + z*z);
+			lastX = x;
+			lastY = y;
+			lastZ = z;
+			float variance = accelCurrent - accelLast;
+			accel = accel * 0.1f + variance; //.1
 
-		  Date time = Calendar.getInstance().getTime();
-			AccelerometerData acc = new AccelerometerData(time, x, y, z);
-			db.beginTransaction();
-			acc = db.copyToRealm(acc);
-			db.commitTransaction();
-			id++;
+			Log.d("Accel Before data: ", Float.toString(accelCurrent));
+			Log.d("Accel data: ", Float.toString(accel));
+			Log.d("Delta data: ", Float.toString(variance));
+
+			if(accel > 0.06){
+				motion.setText("Motion!!!");
+				acclX.setText(Float.toString(accel));
+				entries.add(new Entry(accel, i++));
+				labels.add(Long.toString(curTimeSec));
+			}
+			else{
+				motion.setText("No motion!!");
+			}
+
 		}
 	}
 }
